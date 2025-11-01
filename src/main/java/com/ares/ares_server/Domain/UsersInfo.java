dfe0d742e9ea77d76f1d2c7711e7291bb6d71829
Paul <berindeiepaul8@gmail.com>
2025-11-01 21:59:49 +0200

    Add Hibernate and Liquibase for database management

---
+++ src/main/java/com/ares/ares_server/Domain/UsersInfo.java
@@ -0,0 +1,28 @@
+package com.ares.ares_server.Domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users_info")
+public class UsersInfo {
+    @Id
+    @ColumnDefault("gen_random_uuid()")
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @NotNull
+    @Column(name = "current_area", nullable = false)
+    private Float currentArea;
+
+}
